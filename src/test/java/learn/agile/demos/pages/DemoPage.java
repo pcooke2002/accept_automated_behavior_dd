8d7737a58a490dd8dac5f0c434c1c1118a970e4f
peter.cooke <pcooke2002@yahoo.com>
2019-03-20 03:43:49 -0700

    thread sleep for some browsers on some OS

--- src/test/java/learn/agile/demos/pages/DemoPage.java
+++ src/test/java/learn/agile/demos/pages/DemoPage.java
@@ -50,7 +50,7 @@ public class DemoPage {
     public int getCalculatorResults() throws InterruptedException {
         WebElement resultEl = (new WebDriverWait(driver, 300))
                 .until(ExpectedConditions.presenceOfElementLocated(By.id("calculator-result")));
-        Thread.sleep(100);
+        //Thread.sleep(100);  // timing - for some reason on some OS's webdriver returns too quickly
         return Integer.parseInt(resultEl.getText());
     }
 
