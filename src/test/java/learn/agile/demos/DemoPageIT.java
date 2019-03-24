be4d51fdcc9dad2a2d3caa222929466f72a5b715
peter.cooke <pcooke2002@yahoo.com>
2019-03-24 11:29:37 -0700

    comment out sleep for bowser

--- src/test/java/learn/agile/demos/DemoPageIT.java
+++ src/test/java/learn/agile/demos/DemoPageIT.java
@@ -29,7 +29,7 @@ public class DemoPageIT {
 	public void simpleAddTest() throws Exception {
 		demoPage.go();
 		demoPage.addNumbers(5, 5);
-		Thread.sleep(100);
+		//Thread.sleep(100);  // timing - for some reason on some OS's webdriver returns too quickly
 		assertEquals(demoPage.getCalculatorResults(), 10);
 	}
 
@@ -40,6 +40,7 @@ public class DemoPageIT {
 	public void simpleSubtractTest() throws Exception {
 		demoPage.go();
 		demoPage.subtractNumbers(5, 5);
+		//Thread.sleep(100);  // timing - for some reason on some OS's webdriver returns too quickly
 		assertEquals(0, demoPage.getCalculatorResults());
 	}
 	
@@ -50,6 +51,7 @@ public class DemoPageIT {
 	public void simpleMultiplyTest() throws Exception {
 		demoPage.go();
 		demoPage.multiplyNumbers(5, 5);
+		//Thread.sleep(100);  // timing - for some reason on some OS's webdriver returns too quickly
 		assertEquals(25, demoPage.getCalculatorResults());
 	}
 }
