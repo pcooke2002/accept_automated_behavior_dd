8d6b8ccb3005b61a3a965133bb7c947bed1e2a16
peter.cooke <pcooke2002@yahoo.com>
2019-03-24 11:34:57 -0700

    comment out sleep for bowser

--- src/test/java/learn/agile/demos/DemoPageIT.java
+++ src/test/java/learn/agile/demos/DemoPageIT.java
@@ -29,7 +29,7 @@ public class DemoPageIT {
 	public void simpleSubtractIntegerTest() throws Exception {
 		demoPage.go();
 		demoPage.subtractNumbers(5f, 5f);
-		Thread.sleep(100);
+//		Thread.sleep(100);  // timing - for some reason on some OS's webdriver returns too quickly
 		assert(new Float(0f).equals( new Float(demoPage.getCalculatorResults())));
 		
 	}
@@ -41,7 +41,7 @@ public class DemoPageIT {
 	public void simpleSubtractDecimalTest() throws Exception {
 		demoPage.go();
 		demoPage.subtractNumbers(5.123f, 5.456f);
-		Thread.sleep(100);
+//		Thread.sleep(100);  // timing - for some reason on some OS's webdriver returns too quickly;
 		assert(new Float(-0.3329997f).equals( new Float(demoPage.getCalculatorResults())));
 	}
 }
