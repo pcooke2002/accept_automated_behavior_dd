aa8780541d4e2fa10cabda171fa0fb523e31da3c
peter.cooke <pcooke2002@yahoo.com>
2019-03-31 16:23:12 -0700

    multi browser, fix tests, sleep

--- src/test/java/learn/agile/demos/pages/DemoPage.java
+++ src/test/java/learn/agile/demos/pages/DemoPage.java
@@ -59,7 +59,7 @@ public class DemoPage {
 	public float getCalculatorResults() throws InterruptedException {
 		WebElement resultEl = (new WebDriverWait(driver, 300))
 				.until(ExpectedConditions.presenceOfElementLocated(By.id("calculator-result")));
-		Thread.sleep(100); // timing - for some reason on some OS's webdriver returns too quickly
+//		Thread.sleep(100); // timing - for some reason on some OS's webdriver returns too quickly;
 		return Float.parseFloat(resultEl.getText());
 	}
 
@@ -71,15 +71,12 @@ public class DemoPage {
 		driver.findElement(By.id("first-number")).sendKeys(Float.toString(x));
 		driver.findElement(By.id("second-number")).sendKeys(Float.toString(y));
 		driver.findElement(By.id("subtracter-button")).click();
+
 	}
 
-	/**
-	 * @param f
-	 * @param g
-	 */
-	public void multiplyNumbers(final float f, final float g) {
-		driver.findElement(By.id("first-number")).sendKeys(Float.toString(f));
-		driver.findElement(By.id("second-number")).sendKeys(Float.toString(g));
+	public void multiplyNumbers(float x, float y) {
+		driver.findElement(By.id("first-number")).sendKeys(Float.toString(x));
+		driver.findElement(By.id("second-number")).sendKeys(Float.toString(y));
 		driver.findElement(By.id("multiplyer-button")).click();
 	}
 }
